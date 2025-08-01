f373c78503b9b51b38ed40c39c6cd517a5f608c2
frank940409@gmail.com <frank940409@gmail.com>
2025-08-01 22:24:11 +0800

    fix the 0.0 devision problem and turn PI to constant double type

--- day00/circle.c
+++ day00/circle.c
@@ -7,14 +7,20 @@ int main(){
     float circumference = 0.0f;
     float Surfacearea = 0.0f;
     float volume = 0.0f;
-    double PI = 3.1415926;
+    const double PI = M_PI;//make PI constant double so compiler knows it never change
 
     printf("Enter the radius: ");
     scanf("%f", &r);
 
     circumference = 2.0 * r * PI;
     Surfacearea = pow(r,2) * PI;
-    volume = (4.0/3.0) * (pow(r,3)) * PI;//Can't use 4/3 , 4.0/3.0 instead!
+    volume = (4.0/3.0) * (pow(r,3)) * PI;
+    //Can't use 4/3 , 4.0/3.0 instead!
+    //cuz 3 / 4 = 1. Since is int/int , the answer would be int as well, which is one 
+    //but the fractional part was discarded. 
+    //if a/b, a is double , b is int then the ans would be double,
+    //Else if either is double the ans is double!!!
+    //Else if either is float the ans is float!!!
     
     printf("circumference :%.2f\n",circumference );
     printf("Surfacearea: %.2f\n", Surfacearea);
